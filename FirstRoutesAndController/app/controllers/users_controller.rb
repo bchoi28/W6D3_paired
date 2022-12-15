f14890b3752f2d95a2fe43769361c892cecee1bd
Zane <zeisen33@gmail.com>
2022-12-15 10:14:26 -0500

    Finished First Routes and Controls

--- FirstRoutesAndController/app/controllers/users_controller.rb
+++ FirstRoutesAndController/app/controllers/users_controller.rb
@@ -5,12 +5,39 @@ class UsersController < ApplicationController
     end
 
     def create
-        user = User.new(params.require(:user).permit(:name, :email))
-        user.save!
-        render json: user
+        user = User.new(user_params)
+        if user.save
+            render json: user
+        else 
+            render json: user.errors.full_messages, status: :unprocessable_entity
+        end
     end
 
     def show
-        render json: params
+        @user = User.find(params[:id])
+        render json: @user
+    end
+
+    def update
+        @user = User.find(params[:id])
+
+        if @user.update(user_params)
+            redirect_to user_url(@user)
+        else 
+            render json: @user.errors.full_messages, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        @user = User.find(params[:id])
+        @user.delete
+        index
+        # render json: User.all
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:name, :email)
     end
 end

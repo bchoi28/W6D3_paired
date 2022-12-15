048ed9365307436d4938602e876a344abf526f56
Zane <zeisen33@gmail.com>
2022-12-15 17:29:26 -0500

    Artwork Controller

--- FirstRoutesAndController/app/controllers/users_controller.rb
+++ FirstRoutesAndController/app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
     def create
         user = User.new(user_params)
         if user.save
-            render json: user
+            render json: user, status: :created  
         else 
             render json: user.errors.full_messages, status: :unprocessable_entity
         end
@@ -30,14 +30,12 @@ class UsersController < ApplicationController
 
     def destroy
         @user = User.find(params[:id])
-        @user.delete
-        index
-        # render json: User.all
+        render json: @user.delete
     end
 
     private
 
     def user_params
-        params.require(:user).permit(:name, :email)
+        params.require(:user).permit(:username)
     end
 end
--- FirstRoutesAndController/config/routes.rb
+++ FirstRoutesAndController/config/routes.rb
@@ -1,14 +1,16 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # resources :users
-  get 'users', to: 'users#index', as: 'users'
-  post 'users', to: 'users#create'
-  get 'users/new', to: 'users#new', as: 'new_user'
-  get 'users/:id/edit', to: 'users#edit', as: 'edit_user'
-  get 'users/:id', to: 'users#show', as: 'user'
-  patch 'users/:id', to: 'users#update'
-  put 'users/:id', to: 'users#update'
-  delete 'users/:id', to: 'users#destroy'
+  # get 'users', to: 'users#index', as: 'users'
+  # post 'users', to: 'users#create'
+  # get 'users/new', to: 'users#new', as: 'new_user'
+  # get 'users/:id/edit', to: 'users#edit', as: 'edit_user'
+  # get 'users/:id', to: 'users#show', as: 'user'
+  # patch 'users/:id', to: 'users#update'
+  # put 'users/:id', to: 'users#update'
+  # delete 'users/:id', to: 'users#destroy'
+
+  resources :users, only: [:index, :show, :create, :update, :destroy]
 
   # resources :users
   # Defines the root path route ("/")
